d1be89029058e5ad0e961070f9a66fc2145f2c83
Perry Craft <LjCraft12@gmail.com>
2017-09-28 13:46:56 -0400

    Projects can be saved inside database, titlle still wont pass from the index route to the html, also bootstrap drop down on navbar not working?

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express  = require('express');
-const config   = require('./config/server');
-const exphbs   = require('express-handlebars');
-const mongoose = require('mongoose');
+const express    = require('express');
+const config     = require('./config/server');
+const exphbs     = require('express-handlebars');
+const mongoose   = require('mongoose');
+const bodyParser = require('body-parser');
 
-const app      = express();
+const app        = express();
 
-// Map global promise - get rid of warning (remember to do this regularly
+// Map global promise - get rid of warning (remember to do this regularly)
 mongoose.Promise = global.Promise;
 
 // Connect to Mongoose
@@ -15,6 +16,10 @@ mongoose.connect(config.dbConnection, {
     .then( () =>
         console.log(config.onDbConnectionMessage + config.dbName ))
     .catch(err => console.log(err));
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Load idea model
 require('./models/Idea');
@@ -28,14 +33,50 @@ app.set('view engine', 'handlebars');
 
 // Index Route
 app.get('/', (req, res) => {
-    const title = 'Welcome';
+    const title = 'Welcome BITCH!';
     res.render('index');
-    title: title
+    {
+        title: title
+    }
 });
 
 // About Route
 app.get('/about', (req, res) => {
     res.render('about')
+});
+
+// Add Idea Form
+app.get('/ideas/add', (req, res) => {
+    res.render('ideas/add')
+});
+
+// Proceess Form
+app.post('/ideas', (req, res) => {
+    let errors = [];
+
+    if(!req.body.title) {
+        errors.push({text: 'Please add a title'});
+    }
+    if(!req.body.details) {
+        errors.push({text: 'Please add some details'});
+    }
+    if(errors.length > 0) {
+        res.render('ideas/add',{
+            errors:  errors,
+            title:   req.body.title,
+            details: req.body.details
+        });
+    } else {
+        const newUser = {
+            title:   req.body.title,
+            details: req.body.details
+        };
+        new Idea(newUser)
+            .save()
+            .then(idea => {
+                res.redirect('/ideas');
+            })
+    }
 });
 
 app.listen(config.port, () => {
